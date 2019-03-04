84826a68a8d7def4e556d396563c62e9510b7bf9
Nguyễn Đức Toàn <toanlcgift@gmail.com>
2019-03-05 00:35:53 +0700

    add new project

---
+++ Api/FileController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+
+namespace DatNenWebApi.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileController : Controller
+    {
+        private IHostingEnvironment _hostingEnvironment;
+        private static readonly string UploadFolderName = "Upload";
+
+        public FileController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpPost, DisableRequestSizeLimit]
+        public string UploadFile([FromBody]string content)
+        {
+            try
+            {
+                string folderName = UploadFolderName;
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                string newPath = Path.Combine(webRootPath, folderName);
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+                var bytes = Convert.FromBase64String(content);
+                
+                {
+                    string fullPath = Path.Combine(newPath, "ahihi.pdf");
+                    System.IO.File.WriteAllBytes(fullPath, bytes);
+                }
+                return "Upload Successful.";
+            }
+            catch (System.Exception ex)
+            {
+                return "Upload Failed: " + ex.Message;
+            }
+        }
+
+        [HttpGet]
+        [Route("api/[controller]")]
+        public FileResult downloadFile(string fileName)
+        {
+            string folderName = UploadFolderName;
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string newPath = Path.Combine(webRootPath, folderName);
+            if (!Directory.Exists(newPath))
+            {
+                Directory.CreateDirectory(newPath);
+            }
+            var fullPath = Path.Combine(newPath, fileName);
+            var stream = new FileStream(fullPath, FileMode.Open);
+            var mimeType = "application/x-msdownload";
+            return File(stream, mimeType, fileName);
+        }
+
+        [HttpDelete("{filename}")]
+        public void Delete(string filename)
+        {
+            string folderName = UploadFolderName;
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string newPath = Path.Combine(webRootPath, folderName);
+            if (!Directory.Exists(newPath))
+            {
+                Directory.CreateDirectory(newPath);
+            }
+            System.IO.File.Delete(Path.Combine(newPath, filename));
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace DatNenWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSingleton(Configuration);
+           
+            services.AddSwaggerDocument();
+            
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(opt =>
+                {
+                    opt.SerializerSettings.Converters.Add(new IsoDateTimeConverter());
+                    opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    //opt.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment() || env.IsStaging())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUi3(settings =>
+            {
+                settings.Path = "/api";
+                //settings.DocumentPath = "/api/specification.json";
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
